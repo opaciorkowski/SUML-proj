f58e15f856d01c306baa43e7ab9c290b0ffb8e43
KingaMendyk <s25275@pjwstk.edu.pl>
2024-11-28 20:55:54 +0100

    Create MainPage.py

---
+++ UI/MainPage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Welcome to Vegetable Classifier",
+    page_icon="🍅",
+)
+
+st.sidebar.header("Welcome Page")
+
+st.title("Welcome to Vegetable Classifier!")
+
+#st.sidebar.info("Select a page above")
